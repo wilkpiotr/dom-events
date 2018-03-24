3e5e2d138b633db97418ca43653b1d0d9b7906bb
wilkpiotr <piotrwilk@interia.pl>
2018-03-24 16:07:19 +0100

    pierwsze zmiany

--- src/a.js
+++ src/a.js
@@ -2,13 +2,27 @@ console.log('I am a.js!');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
+  counting();
 });
 
 // A.1 - Stworz licznik ktory nalicza numery co 1 sekunde i wstawia liczbe to html
+const counting = () => {
+  const counter = document.querySelector('section > label');
+  let  number = 0;  
+setInterval(() => {
+  number++;
+  counter.innerText = 'Counter: ' + number;
+},1000)
+}
+
 // A.2 - Stworz minutnik ktory posiada strukture 00:00 - inkrementuje sekundy, pokazuje ile uplynelo minut i sekund
+
+const minuting = () => {
+  const timer = document.querySelector('#timer')
+}
 // A.3 - Stworz funkcje ktora na wejsciu dostaje co ile minutnik ma sie inkrementowac - po wywolaniu
 // ma dodac do strony kolejny minutnik ktory inkrementuje zgodnie z zadanym argumentem
 // A.4 - Dodaj mozliwość ustawienia maksymalnej liczby minutnika po ktorej minutnik ma sie zatrzymac
 // A.5 - Dodaj mozliwosc ustawienia czasu dzialania minutnika po ktorym minutnik stanie
 
-// A.6 - Dodaj przycisk zatrzymujacy i uruchamiajacy minutnik
+// A.6 - Dodaj przycisk zatrzymujacy i uruchamiajacy minutnik 
--- src/b.js
+++ src/b.js
@@ -2,12 +2,44 @@ console.log('I am b.js!');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
+  b1();
+  b3();
 });
 // B.1 - Za każdym kliknięciem na dowolny przycisk dopisz do diva clicked-info data-id kliknietego przycisku.
+const b1 = () => {
+  const buttons = document.querySelectorAll('body button');
+  const divInfo = document.querySelector('#clicked-info');
+  const buttonArr = Array.from(buttons);
+  buttonArr.forEach((button) => {
+    button.addEventListener('click',(e) => {
+      divInfo.innerText =  divInfo.innerText + ' ' + button.dataset.id;
+  })
+})
+}
+
 // B.2 - Zapisuj ile razy uzytkownik klika w dany przycisk i pokaz taka infomacje w formacie A: 3, B: 2, C: 0, gdzie
 // format odpowiada {data-id przycisku}: {ilosc klikniec}
+const b2 = () => {
+
+}
+
 // B.3 - dodaj na stronie wiecej przyciskow. Po kliknieciu w przycisk o danym data-id wszystkie przyciski z tym data-id
 // maja zniknac ze strony
+
+const b3 = () => {
+  const body = document.querySelector('body');
+  document.querySelector('body').addEventListener('click',(e) => {
+    if (!e.target.hasAttribute('data-id')) {
+     return;
+    }
+    const dataId = e.target.dataset.id;
+    const buttons = document.querySelectorAll('button[data-id"'+dataId+'"]');
+
+    buttons.forEach((button) => {
+      body.removeChild(button);
+    })
+  })
+}
 // B.4 - po kliknieciu w przycisk o id = add dodac nowy div z tekstem wpisanym w input
 // B.5 - rozszerzenie B.4 - czysicic inputa po kazdym przycisnieciu przycisku
 // B.6 - po kliknieciu w dowolny przycisk A, B, C ma pojawic sie nowy przycisk i ma rowniez obslugiwac kliniecia
