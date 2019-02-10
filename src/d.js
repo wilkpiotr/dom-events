b1a28d092b8626cf8c4ec345b8288a6b7d0769f0
Piotr Wilk <piotrwilk@interia.pl>
2019-02-10 22:19:50 +0100

    section d finished to d.9 task

--- src/d.js
+++ src/d.js
@@ -2,26 +2,85 @@ console.log('I am d.js!');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
-  d1();
+  d();
+  
 });
 
 // D.1 - Obsluz przycisk dodawania zadania - wartosc wpisana w textarea ma byc dodana do listy
 
-const d1 = () => {
+const d = () => {
   const ul = document.querySelector('ul');
   const button = document.querySelector('button');
-  const task = document.querySelector('#new-task');
+  const textarea = document.querySelector('#new-task');
+  const taskCounter = document.querySelector('#task-counter');
+  const noTaskInfo = ul.innerHTML;
+  // let allTasks = 0;
+
   button.addEventListener('click', (e) => {
-    if (ul.firstElementChild.id === 'no-tasks') {
-      ul.removeChild(ul.firstElementChild);
-    }
-    
-    const toDo = document.createElement('li');   
-    toDo.innerText = task.value;
-    ul.appendChild(toDo);
-    task.value = '';
+    if (document.querySelector('#no-tasks')) {
+      ul.removeChild(ul.firstElementChild)
+    } 
+    addTask();
+    countTask();
   })
+
+  const addTask = () => {
+    if (textarea.value !== '') {
+    const task = document.createElement('li');
+    task.innerHTML = `${textarea.value} <a id="delete-me">delete</a> <a id="move-up">&#8657</a> <a id="move-down">&#8659;</a><input type="checkbox">`;
+    ul.appendChild(task);
+    textarea.value = '';
+    }
+  }
+
+  const deleteTask = () => {
+    ul.addEventListener('click', (e) => {
+    if (e.target.id === 'delete-me') {
+      ul.removeChild(e.target.parentElement)
+      countTask()
+      } 
+    })
+  };
+
+  const checkTask = () => {
+    ul.addEventListener('click', (e) => {
+      if (e.target.tagName === 'INPUT') {
+        e.target.parentElement.classList.toggle('task-done');
+      } 
+      countTask();
+    })
+  }
+
+  const moveTask = () => {
+    ul.addEventListener('click', (e) => {
+      if (e.target.id === 'move-up' && e.target.parentElement.previousElementSibling !== null) {
+        e.target.parentElement.parentElement.insertBefore(e.target.parentElement, e.target.parentElement.previousElementSibling );
+      } 
+      else if (e.target.id === 'move-down' && e.target.parentElement.nextElementSibling !== null) {
+        e.target.parentElement.parentElement.insertBefore(e.target.parentElement.nextElementSibling, e.target.parentElement );
+      } 
+    })
+  }
+
+  const countTask = () => {
+    const list = Array.from(document.querySelectorAll('li'))
+    const doneList = Array.from(document.querySelectorAll('li.task-done'));
+    taskCounter.innerText = 'All tasks: ' + doneList.length + "/" + list.length;
+    noTasks();
+  }
+
+  const noTasks = () => {
+    const list = Array.from(document.querySelectorAll('li'))
+    if (list.length === 0) {
+      ul.innerHTML = noTaskInfo;
+    }
+  }
+
+  deleteTask();
+  checkTask();
+  moveTask();
 }
+
 // D.2 - Element li o id = no-task powinien zniknac jak zadanie zostaje dodane
 // D.3 - Textarea new-task powinien miec czyszczona zawartosc po dodaniu zadania
 // D.4 - Dodaj link (znacznik a) do zadania na liscie i usun zadanie z listy po jego kliknieciu
